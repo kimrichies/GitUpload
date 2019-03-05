a15450237a93ad2842aa606a9c86d42f5d63aba4
kimrichies <kimrichies@gmail.com>
2019-03-05 20:41:59 +0300

    The first commit on this project

--- app/src/main/java/ug/co/must/kimrichies/gitupload/MainActivity.java
+++ app/src/main/java/ug/co/must/kimrichies/gitupload/MainActivity.java
@@ -1,7 +1,9 @@
 package ug.co.must.kimrichies.gitupload;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scroll(View v1){
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        startActivity(intent);
     }
 }
